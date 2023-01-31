87108c49cc001bdfe5a9f6f86e16e887817dc2ef
elsa sabilah <elsabilah1@gmail.com>
2023-01-31 14:11:34 +0700

    initial setup

---
+++ README.md
@@ -0,0 +1,1 @@
+## Jatidiri Multifarm
---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Jatidiri Multifarm</title>
+        <meta name="description" content="Jatidiri Multifarm" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>Jatidiri Multifarm</h1>
+      </main>
+    </>
+  )
+}
