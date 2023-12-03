5e90ea39bb49921b703a45e58152aac72d7347f1
reizha <reizha77@gmail.com>
2023-12-03 23:34:13 +0700

    fix-dev:fixing api

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/v1/:path*",
+        source: "/api/:path*",
         destination: `${process.env.API_BASE_URL}/:path*`,
       },
     ]
